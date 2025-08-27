72573a2ed45ba4531c3d6387539906196cc3204a
uuaxe <cweixin@gmail.com>
2025-08-27 17:52:18 +0800

    plot average across all samples in group for PepMapViz

--- notebooks/peptides.R
+++ notebooks/peptides.R
@@ -1,43 +1,83 @@
 library(data.table)
+library(dplyr)
 library(ggplot2)
-library(PepMapViz)
 library(jsonlite)
+library(magrittr)
+library(PepMapViz)
 
 
 # Require: sample_id, class, timepoint, batch(?)
 # Protein.Group, Genes, Precursor.Charge, Precursor.Normalised
-file <- 'data/astral/processed/report-matched-M0-P19827.csv'
+file <- 'data/tmp/spliceoforms/report-matched-M0.csv'
 report <- read.csv(
   file, header = TRUE,
   stringsAsFactors = FALSE, check.names = FALSE
 )
 data <- as.data.table(report)
 
+# Check data
+head(data)
+dim(data)
+length(unique(data$Polypeptide.Novogene.ID))
+
+data$label[
+  data$label %in% c("maintain", "early_remit", "late_remit", "relapse", "remit")
+] <- "Non-convert"
+data$label[data$label == "convert"] <- "Convert"
+
 # Exon information
 file <- 'data/tmp/spliceoforms/itih1-1.json'
 itih1_1 <- fromJSON(file)
 print(itih1_1)
-names(itih1_1)
+
+file <- 'data/tmp/spliceoforms/itih4-1.json'
+itih4_1 <- fromJSON(file)
+print(itih4_1)
+
+file <- 'data/tmp/spliceoforms/serpinf2-1.json'
+serpinf2_1 <- fromJSON(file)
+print(serpinf2_1)
+
+file <- 'data/tmp/spliceoforms/serpinf2-2.json'
+serpinf2_2 <- fromJSON(file)
+print(serpinf2_2)
+
+file <- 'data/tmp/spliceoforms/kng1-1.json'
+kng1_1 <- fromJSON(file)
+print(kng1_1)
+
+file <- 'data/tmp/spliceoforms/kng1-2.json'
+kng1_2 <- fromJSON(file)
+print(kng1_2)
 
 # Skipped functions: strip_sequence_DIANN, obtain_mod_DIANN 
 # No need to strip sequence as we have that already
 # No need to obtain PTM because we do not want to plot
 
+gene_symbol <- "ITIH4"
+print(gene_symbol %in% unique(data$Genes))
+
+# Filter data for just peptides mapping to gene of interest
+data_fltr <- data[data$Genes == gene_symbol, ]
+head(data_fltr)
+dim(data_fltr)
+
 whole_seq <- data.frame(
-  Region_Sequence = c(itih1_1$sequence),
-  Genes = c("ITIH1")
+  Region_Sequence = c(itih4_1$sequence),
+  Genes = c(gene_symbol)
 )
 print(whole_seq)
 
+# Matches all peptide sequences to the reference sequence provided 
 matching_result <- match_and_calculate_positions(
-  data,
+  data_fltr,
   column = "Stripped.Sequence", # name of column with sequence
   whole_seq,
   match_columns = c("Genes"), # match column from data with whole_seq
-  column_keep = colnames(report)[1:3] # information to keep 
+  column_keep = c("Polypeptide.Novogene.ID", "label") # information to keep 
 )
-head(matching_result)
-head(whole_seq)
+# colnames(matching_result)[1] <- "Sequence" # To avoid error
+dim(matching_result)
 
 # TODO: Raise issue - Allow matching_result to not have reps and PTM_position
 matching_result$reps <- NA
@@ -45,44 +85,76 @@ matching_result$PTM_position <- NA
 data_with_quantification <- peptide_quantification(
   whole_seq,
   matching_result,
-  matching_columns = c("Genes"),
-  distinct_columns = colnames(report)[1:3], # not aggregated for these conditions
-  quantify_method = "PSM",
+  matching_columns = c("Genes"), # identifying columns for sequences in whole_seq
+  distinct_columns = c("Polypeptide.Novogene.ID", "label"), # not aggregated for these conditions
+  quantify_method = "PSM", # PSM or Area
   # area_column = "Precursor.Normalised",
   with_PTM = FALSE,
   reps = FALSE
 )
-head(data_with_quantification)
+dim(data_with_quantification)
+
+# file <- 'tmp/astral/data_with_quantification.csv'
+# write.csv(data_with_quantification, file, row.names = TRUE)
+
+# Average PSMs across samples from the same label
+data_avg <- data_with_quantification %>%
+  select(-Genes, -Polypeptide.Novogene.ID) %>%
+  group_by(Character, Position, label) %>%
+  summarise(PSM = mean(PSM, na.rm = TRUE), .groups = "keep") %>%
+  arrange(label, Position) %>%
+  as.data.table()
 
 # Domain information
-domain <- itih1_1$exons
-domain["domain_color"] <- c(rep("#F8766D", 8), rep("#B79F00", 14)) 
-domain
+domain <- itih4_1$exons
+domain_color <- c(
+  rep("#F8766D", 14),
+  rep("#B79F00", 2),
+  # rep("#00BA38", 1),
+  # rep("#B79F00", 1),
+  rep("#F8766D", 8)
+  # rep("#00BFC4", 1),
+  # rep("#619CFF", 1),
+  # rep("#F564E3", 15)
+)
+domain$domain_color <- domain_color
+head(domain)
 
+# For samples grouped by label. It will plot the last sample label by means
+# of geom_raster, and does not aggregate across samples.
 p <- create_peptide_plot(
-  data_with_quantification,
+  data_avg,
   y_axis_vars = c("label"),
-  x_axis_vars = NULL, # Facets according to region. Option: NULL 
-  y_expand = c(0.2, 0.2),
-  x_expand = c(0.5, 0.5),
+  x_axis_vars = NULL, # Facets according to region.
+  y_expand = c(0.8, 0.8),
+  x_expand = c(0, 0),
   theme_options = list(),
-  labs_options = list(title = "PSM Plot", x = "Position", fill = "PSM"),
+  labs_options = list(title = "ITIH4-1", x = "Position"),
   color_fill_column = 'PSM',
   fill_gradient_options = list(),
-  label_size = 5,
+  label_size = 3,
   add_domain = TRUE, # TODO: Even if FALSE, domain will be plotted
   domain = domain, # TODO: ERROR if domain not provided
   domain_start_column = "start",
   domain_end_column = "end",
   domain_type_column = "exon",
   domain_fill_color_column = "domain_color",
+  domain_label_size = 2.5, 
   # PTM = FALSE,
   # PTM_type_column = "PTM_type",
   # PTM_color = PTM_color,
-  # add_label = TRUE,
+  add_label = FALSE,
   # label_column = "Character",
-  # column_order = list(Region_1 = 'VH,VL')
+  # column_order = list(Region_1 = 'VH, VL')
 )
-file = "tmp/astral/fig/spliceoforms/peptides-ITIH1-id.pdf"
-ggsave(p, filename = file, width = 10, height = 20)
+file = "tmp/astral/fig/spliceoforms/peptides-ITIH4-1-PSM.pdf"
+ggsave(p, filename = file, width = 12, height = 2.5)
+
+
+# # Count number of labels
+# data %>%
+#   select(Polypeptide.Novogene.ID, label) %>%
+#   unique() %>%
+#   group_by(label) %>%
+#   summarise(count = n())
 
